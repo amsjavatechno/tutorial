c00f7779f50cc5bceeefb611f250549a5c414e10
amsjavtechno <amsjavtechno@gmail.com>
2023-02-27 00:10:32 +0530

    chapter 2 done

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,71 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("AMS PORTAL"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("This is Container Body"),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              // shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(66, 222, 91, 91),
+                  offset: Offset(2.0, 5.0),
+                  blurRadius: 5.0,
+                )
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
+            ),
+            child: Text("This is Container Body"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              // shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(66, 222, 91, 91),
+                  offset: Offset(2.0, 5.0),
+                  blurRadius: 5.0,
+                )
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
+            ),
+            child: Text("This is Container Body"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              // shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(66, 222, 91, 91),
+                  offset: Offset(2.0, 5.0),
+                  blurRadius: 5.0,
+                )
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
+            ),
+            child: Text("This is Container Body"),
+          ),
+        ],
       ),
     );
   }
