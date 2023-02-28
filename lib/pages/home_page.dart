749d17c2e4b16515bf817f38626b5fce06fac864
amsjavtechno <amsjavtechno@gmail.com>
2023-02-28 11:03:57 +0530

    new Changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
-  var myText = "Change Me";
+  var myText = "Change me with Refresh";
 
   @override
   void initState() {
